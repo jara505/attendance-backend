593580497393a158d32ceb10348bf4400c925f9a
jara505 <juanignaciojara505@gmail.com>
2026-05-28 21:36:17 -0600

    chore/database-migration (#22)

--- src/main.py
+++ src/main.py
@@ -32,8 +32,7 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=".*",
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
